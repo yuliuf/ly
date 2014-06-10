93473742735ccef7c1d3332c2c2fa96eca1dba7e
liuyu <lanou3g@liuyu.local>
2014-06-10 16:39:09 +0800

    1.0
    项目第一天

---
+++ First/First/LYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LYAppDelegate.h
+//  First
+//
+//  Created by liuyu on 14-6-10.
+//  Copyright (c) 2014年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ First/First/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  First
+//
+//  Created by liuyu on 14-6-10.
+//  Copyright (c) 2014年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LYAppDelegate class]));
+    }
+}
